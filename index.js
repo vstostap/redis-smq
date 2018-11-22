344884982fecec3baed42e1c27378a13f298cbab
ovasiuta <vstostap@gmail.com>
2018-11-22 16:32:40 +0200

    Remove monitor

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
 const Consumer = require('./src/consumer');
 const Producer = require('./src/producer');
 const Message = require('./src/message');
-const monitor = require('./src/monitor');
 
 module.exports = {
     Consumer,
     Producer,
     Message,
-    monitor,
 };
-
